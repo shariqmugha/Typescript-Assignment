306f57e287fb6e02ceff5323ce8301334a3e9770
shariqmugha <168197177+shariqmugha@users.noreply.github.com>
2024-04-29 05:50:17 -0700

    Add files via upload

---
+++ Greetings.ts
@@ -0,0 +1,9 @@
+
+
+let names :string[] =['shariq mughal','saqib mughal','iqbal mughal','ahmed','moiz'] ;
+
+const message :string = 'welcome to typescript class :'
+
+for(let i=0; i<names.length; i++){
+    console.log(message + names[i]) ;
+}
