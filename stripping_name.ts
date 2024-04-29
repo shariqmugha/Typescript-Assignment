507358e7afe9594f45ba846e2252634ef3666063
shariqmugha <168197177+shariqmugha@users.noreply.github.com>
2024-04-29 05:45:31 -0700

    Add files via upload

---
+++ stripping_name.ts
@@ -0,0 +1,13 @@
+let personName:string = '   \n\tshariq mughal\t\n    ';
+console.log(personName);
+
+let whitout_whitespace:string = personName.trim();
+console.log(whitout_whitespace);
+
+
+
+
+
+
+
+//
