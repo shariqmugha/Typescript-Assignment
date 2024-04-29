d173c29e8be55d90eb2e6d29cdcae2a6073bdc41
shariqmugha <168197177+shariqmugha@users.noreply.github.com>
2024-04-29 05:57:23 -0700

    Add files via upload

---
+++ alien_colors_2.ts
@@ -0,0 +1,14 @@
+let alienColors: string = "green";
+if(alienColors === "green "){
+    console.log("player just earned 5 point for shooting the alien.");
+
+}else{
+    console.log("player just earned 5 points.")
+    }
+alienColors = "yellow"
+
+if(alienColors === "green"){
+    console.log("player just earned 5 point for shooting the alien.");
+}else{
+    console.log("player just earned 10 points.")
+}
