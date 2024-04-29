7b5cf4b1b58f115e55f6574ea15c9e7e34a01faa
shariqmugha <168197177+shariqmugha@users.noreply.github.com>
2024-04-29 05:54:35 -0700

    Add files via upload

---
+++ Empty-Array.ts
@@ -0,0 +1,13 @@
+
+//think of something you could store in a array.
+let items :string[] = ['pakistan','karachi','sindh'];
+
+//write a program that creates alist containing these iteams.
+
+let item2 :string[] = [] ;
+
+item2.push('Germany');
+item2.push('England');
+item2.push('Indian');
+
+console.log(item2);
