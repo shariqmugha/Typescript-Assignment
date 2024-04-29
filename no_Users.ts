52b9a08c0f6a6b2b1490f80ba4ee7c5d2fc3fa13
shariqmugha <168197177+shariqmugha@users.noreply.github.com>
2024-04-29 06:08:07 -0700

    Add files via upload

---
+++ no_Users.ts
@@ -0,0 +1,9 @@
+let userNames:string[] = ['admin','saqib','iqbal','shariq','basit'];
+
+if(userNames.length === 0){
+    console.log('we need to find some users!');
+}else{
+    userNames = [];
+    console.log('All user Names have been removed. ' + userNames.length);
+}
+
