cb292f4bd0952cbfb9d3d4509e7407a4fdcee0b8
shariqmugha <168197177+shariqmugha@users.noreply.github.com>
2024-04-29 06:17:26 -0700

    Add files via upload

---
+++ large_Shirts.ts
@@ -0,0 +1,8 @@
+function make_shirt(size:string = 'Large', text:string ="I love typescript."){
+   console.log(`cerating a ${size} shirt with the message ${text}`);
+}
+make_shirt();
+
+make_shirt('medium');
+
+make_shirt('Small','I love python');
