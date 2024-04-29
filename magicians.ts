6fe82bf3a6063ee6fc8e1aad6d909a12092da383
shariqmugha <168197177+shariqmugha@users.noreply.github.com>
2024-04-29 06:19:26 -0700

    Add files via upload

---
+++ magicians.ts
@@ -0,0 +1,11 @@
+let magician: string[] = ['Harry potter','Hermione Granger','Ron Wesly','Albus Dumbledore'];
+
+function show_magicians(magicians: string[]){
+
+    magicians.forEach(element => {
+        console.log(element);
+    });
+}
+
+
+show_magicians(magician)
