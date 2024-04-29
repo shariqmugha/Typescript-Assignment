c120425a7f3539f02368cdec2b5aa138be306999
shariqmugha <168197177+shariqmugha@users.noreply.github.com>
2024-04-29 06:16:57 -0700

    Add files via upload

---
+++ t_Shirt.ts
@@ -0,0 +1,11 @@
+// call the function.
+
+function make_shirt(size:string, text:string){
+    console.log(`creating a ${size} shirt with the message: ${text}`);
+}
+
+make_shirt('large','hello mughal');
+
+make_shirt('medium','hello mughal');
+
+make_shirt('small','hello mughal');
