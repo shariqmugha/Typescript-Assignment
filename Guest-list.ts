ba5877584b8e74db692b9e854de3388a896d1dd8
shariqmugha <168197177+shariqmugha@users.noreply.github.com>
2024-04-29 05:51:27 -0700

    Add files via upload

---
+++ Guest-list.ts
@@ -0,0 +1,8 @@
+
+
+let Guest_list :string[] =['imram khan','Nawaz sharif','asif ali zardari'];
+
+for(let i=0; i<Guest_list.length; i++){
+    console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank you!')
+
+}
