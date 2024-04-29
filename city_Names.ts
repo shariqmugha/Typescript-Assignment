f2774cf054273d23b0e866459043a1793cc052fc
shariqmugha <168197177+shariqmugha@users.noreply.github.com>
2024-04-29 06:18:32 -0700

    Add files via upload

---
+++ city_Names.ts
@@ -0,0 +1,9 @@
+function city_country(city: string, country: string){
+  return `${ city}, ${country}`;
+}
+
+console.log(city_country('karachi','pakistan'));
+
+console.log(city_country('tokyo','japan'));
+
+console.log(city_country('paris','france'));
